cd0748e75d160b077c5976a35d33148c7a7bb3ca
vivgg <vivog2017@gmail.com>
2022-07-07 14:45:14 +0300

    some addings

--- my_site/site_app/urls.py
+++ my_site/site_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('download_cv/', download_cv, name='downloadcv'),
     path('skills/', SkillsView.as_view(), name='skills'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    # path('contacts/', send_email, name='send_email'),
+    path('portfolio/', portfolio, name='portfolio'),
 
 
 ]
