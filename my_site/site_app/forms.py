23db683e93deb5eaf1ff8b8b8daa59a5e4defb68
vivgg <vivog2017@gmail.com>
2022-07-19 15:45:41 +0300

    more

--- my_site/site_app/forms.py
+++ my_site/site_app/forms.py
@@ -2,16 +2,31 @@ from django import forms
 from django.forms import ModelForm, TextInput, EmailInput
 
 from .models import MainInfo
+# FIO = 'Савушкін Віталій'
+FIO = 'Savushkin Vitalii'
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Ім\'я', max_length=50,
+    if FIO == 'Савушкін Віталій':
+        name = forms.CharField(label='Ім\'я', max_length=50,
                            widget=TextInput(
                                attrs={'class': 'form-control', 'placeholder': "ІМ\'Я", 'style': 'font-size: 14px'}))
-    email = forms.EmailField(label='Email', max_length=150,
+        email = forms.EmailField(label='Email', max_length=150,
                              widget=EmailInput(attrs={'class': 'form-control', 'placeholder': "ВАША ПОШТОВА СКРИНЬКА",
                                                       'style': 'font-size: 14px'}))
-    message = forms.CharField(label='Повідомлення', max_length=5000,
+        message = forms.CharField(label='Повідомлення', max_length=5000,
                               widget=forms.Textarea(
                                   attrs={'size': 100, 'class': 'form-control', 'placeholder': "ПОВІДОМЛЕННЯ",
                                          'style': 'font-size: 14px'}))
+    else:
+        name = forms.CharField(label='Ім\'я', max_length=50,
+                               widget=TextInput(
+                                   attrs={'class': 'form-control', 'placeholder': "NAME", 'style': 'font-size: 14px'}))
+        email = forms.EmailField(label='Email', max_length=150,
+                                 widget=EmailInput(
+                                     attrs={'class': 'form-control', 'placeholder': "EMAIL",
+                                            'style': 'font-size: 14px'}))
+        message = forms.CharField(label='Повідомлення', max_length=5000,
+                                  widget=forms.Textarea(
+                                      attrs={'size': 100, 'class': 'form-control', 'placeholder': "MESSAGE",
+                                             'style': 'font-size: 14px'}))
